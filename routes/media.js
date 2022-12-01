a37acb99a573a833a59b68935fe1b028e05f8048
isalzufari <isalzufari@gmail.com>
2022-12-01 12:06:05 +0700

    integrated service media

--- routes/media.js
+++ routes/media.js
@@ -5,5 +5,7 @@ const mediaHandler = require('./handler/media');
 
 /* GET users listing. */
 router.post('/', mediaHandler.create);
+router.get('/', mediaHandler.getAll);
+router.delete('/:id', mediaHandler.destroy);
 
 module.exports = router;
