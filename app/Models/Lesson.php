25261455e58eaaefadf1dd1dea1c7ae0efe4da60
isalzufari <isalzufari@gmail.com>
2023-01-31 21:40:59 +0700

    lesson

--- app/Models/Lesson.php
+++ app/Models/Lesson.php
@@ -8,7 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Lesson extends Model
 {
     protected $table = 'lessons';
-
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:m:s',
+        'updated_at' => 'datetime:Y-m-d H:m:s',
+    ];
     protected $fillable = [
         'name',
         'video',
