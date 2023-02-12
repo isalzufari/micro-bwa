afbaa3537db5bf13f772e402275abce009467b86
isalzufari <isalzufari@gmail.com>
2023-02-13 06:27:42 +0700

    order & webhook

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\WebhookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('orders', [OrderController::class, 'create']);
+Route::get('orders', [OrderController::class, 'index']);
+
+Route::post('webhook', [WebhookController::class, 'midtransHandler']);
