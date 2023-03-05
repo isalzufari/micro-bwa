658808563531b8202fab6b96f53909e64abadcac
isalzufari <isalzufari@gmail.com>
2023-03-05 18:29:09 +0700

    middleware "permission"

--- routes/handler/media/create.js
+++ routes/handler/media/create.js
@@ -11,6 +11,8 @@ module.exports = async (req, res) => {
     return res.json(media.data);
   } catch (error) {
 
+    console.log(error);
+
     if (error.code === "ECONNREFUSED") {
       return res.status(500).json({
         status: 'error',
